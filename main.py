2f741fdf7e1c1cca98f1e19fd56365038c729858
ArthasReyes <fer.reyesl@gmail.com>
2025-02-09 04:08:57 -0300

    Update examples on main.py
    Add use of function with params on webpage

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from time import sleep_ms
 from machine import Pin, PWM
 
 led = Pin("LED", Pin.OUT)
+def blink_time(time):
+  led.toggle()
+  sleep_ms(time)
+  led.toggle()
+  sleep_ms(time)
 
+def multiblink(times):
+  for i in range(times):
+    led.toggle()
+    sleep_ms(time)
+    
 page = Page()
-page.add_action_component('Change Light', multiblink)
+page.add_action_component('Change Light', led.toggle)
 page.add_on_off_component('LED integrado:', led.on, led.off)
-page.add_range_value_component('Freq is: ', 0, 1000, 500, multiblinktime)
-page.add_range_value_component('Freq is: ', 1, 10, 500, multiblinktime2)
+page.add_range_value_component('Blink times : ', 1, 10, 1, multiblink)
+page.add_range_value_component('Time of blink: ', 0, 1000, 500, blink_time)
 
 Server("Hachiko_24", "iquique47", page)
