5534b636a56bc623c3420b3597d40c8c9e788ad9
ArthasReyes <fer.reyesl@gmail.com>
2025-11-22 21:58:03 -0300

    Hacer poeticiones asincronas al server

--- example_generic.py
+++ example_generic.py
@@ -1,24 +1,24 @@
-from web import Server, Page
-import sys
-from time import sleep_ms
+from web import Server, Page, Wifi
+from asyncio import sleep, sleep_ms, run
 from machine import Pin, PWM
 
 led = Pin("LED", Pin.OUT)
-def blink_time(time):
-  led.toggle()
-  sleep_ms(time)
-  led.toggle()
-  sleep_ms(time)
-
-def multiblink(times):
-  for i in range(times):
-    led.toggle()
-    sleep_ms(time)
     
+async def multiblink(times):
+    for _ in range(times*2):
+        led.toggle()
+        await sleep(0.5)
+        
 page = Page()
-page.add_action_component('Change Light', led.toggle)
-page.add_on_off_component('LED integrado:', led.on, led.off)
-page.add_range_value_component('Blink times : ', 1, 10, 1, multiblink)
-page.add_range_value_component('Time of blink: ', 0, 1000, 500, blink_time)
+page.add_action_component("Luces", led.toggle)
+page.add_on_off_component("Luces", led.on, led.off)
+page.add_range_value_component("Multiblink", 1, 10, 5, multiblink)
 
-Server("Hachiko_24", "iquique47", page)
+
+async def main():
+    Wifi("Hachiko_24", "iquique47")
+    server = Server(page)
+    await server.start_server()
+
+if __name__ == "__main__":
+    run(main())
