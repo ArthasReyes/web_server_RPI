8cbc251aae2bb3a478504119683ca9c86e0cbf7e
ArthasReyes <fer.reyesl@gmail.com>
2025-11-22 21:58:33 -0300

    Hacer poeticiones asincronas al server

--- example_my_html_css.py
+++ example_my_html_css.py
@@ -1,27 +1,23 @@
-from web import Server, Page
-from time import sleep_ms
+from web import Server, Page, Wifi
+from asyncio import sleep, sleep_ms, run
 from machine import Pin, PWM
 
 led = Pin("LED", Pin.OUT)
-def blink_time(time):
-  led.toggle()
-  sleep_ms(time)
-  led.toggle()
-  sleep_ms(time)
-
-def multiblink(times):
-  for _ in range(times):
-    led.toggle()
-    sleep_ms(time)
 
 with open("style.css", "r") as f:
     my_css = f.read()
 
-with open("style.css", "r") as f:
+with open("site.html", "r") as f:
     my_html = f.read()
 
-#Conecto las acciones a los componentes
 page = Page(html = my_html, css= my_css)
 page.add_action_component("EncenderLED", led.on)
 page.add_action_component("ApagarLED", led.off)
-Server("Hachiko_24", "iquique47", page)
+
+async def main():
+    Wifi("Hachiko_24", "iquique47")
+    server = Server(page)
+    await server.start_server()
+
+if __name__ == "__main__":
+    run(main())
